0d81b518c8b1b14b0fa56701708c00e2125ccb2a
Eduardo <edalvarez2015@gmail.com>
2020-12-29 16:32:42 -0600

    Understanding arrays - project completed!
    Perfect Practice Makes Perfect

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you',
  'get', 'easily', 'the', 'first', 'time,', 'it', 'is',
-  'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,',
-   'Chris', 'Pine,', 'Learn', 'JavaScript'];
+ 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,',
+ 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+secretMessage.pop();
+secretMessage.push('to', 'program');
+secretMessage[7] = 'right';
+secretMessage.shift();
+secretMessage.unshift('programming');
+secretMessage.splice(6, 5, 'know');
+console.log(secretMessage.join(' '));
